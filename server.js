dec3be283c29398bb3cbeeeb233d04e70f145fa4
cis101 <cis101@secs.iub.edu.bd>
2019-07-10 08:43:10 +0600

    CW 8b

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('index.html', function(err, data){
-      if(err){
-          return console.log("File read error")
-      }
-      console.log(data);
-      res.end(data);
-  })
-});
+var express = require('express');
+var app = express();
+var server= http.Server(app)
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.get('/second',function(req,res){
+    res.sendFile(__dirname+'/second.html')// var http = require('http');
+  })  
+// var fs = require('fs');
+// var server = http.createServer(function(req, res){
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   fs.readFile('index.html', function(err, data){
+//       if(err){
+//           return console.log("File read error")
+//       }
+//       console.log(data);
+//       res.end(data);
+//   })
+// });
 server.listen(3000, 'localhost', function(){
   console.log('Server running');
 });
